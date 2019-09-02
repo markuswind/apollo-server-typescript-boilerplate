bafd4e46d9d64ba1be1c91db1d05c36dc3f2027e
Markus Wind <markuswind@gmail.com>
2019-09-02 10:47:03 +0200

    chore: added prettier

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from 'apollo-server';
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
 const books = [
   {
-    title: "Harry Potter and the Chamber of Secrets",
-    author: "J.K. Rowling"
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
   },
   {
-    title: "Jurassic Park",
-    author: "Michael Crichton"
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
   }
 ];
 
