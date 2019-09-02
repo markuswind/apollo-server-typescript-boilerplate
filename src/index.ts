c894a06ccbca1097334c5535724ab6a7131ab775
Markus Wind <markuswind@gmail.com>
2019-09-02 10:40:43 +0200

    chore: added typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
